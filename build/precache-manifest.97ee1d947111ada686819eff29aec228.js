a941c66b6341efce32e137e8720ac550cf2317b6
shomail <shumailmehmood1@gmail.com>
2020-06-10 13:37:23 +0500

    C

---
+++ build/precache-manifest.97ee1d947111ada686819eff29aec228.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317525092c8c7a6b5cb6d4283d638be8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0293ac6dfe91933f210e",
+    "url": "/static/css/main.126c14d6.chunk.css"
+  },
+  {
+    "revision": "eb747399eb5e3d8648e0",
+    "url": "/static/js/2.b61d13f0.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.b61d13f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0293ac6dfe91933f210e",
+    "url": "/static/js/main.44802a37.chunk.js"
+  },
+  {
+    "revision": "ab4ff4dc57d7fd279b8e",
+    "url": "/static/js/runtime-main.389941f4.js"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  }
+]);
