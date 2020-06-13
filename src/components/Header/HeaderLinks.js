8eedac59885c75553beb3f5aca20ffcf4b8ef7e9
unknown <shumailmehmood1@gmail.com>
2020-06-13 17:18:50 +0500

    C

--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -36,60 +36,62 @@ export default function HeaderLinks(props) {
     props.filterBy(val)
   };
   return (
-    <List className={classes.list}>
-      <ListItem className={classes.listItem}>
-        {/*<Tooltip title="Delete">
-          <IconButton aria-label="Delete">
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>*/}
-        <Tooltip
-          id="instagram-twitter"
-          title="Follow us on twitter"
-          placement={window.innerWidth > 959 ? "top" : "left"}
-          classes={{ tooltip: classes.tooltip }}
-        >
-          <Button
-            href="#"
-            color="transparent"
-            className={classes.navLink}
-          >
-            <i className={classes.socialIcons + " fab fa-twitter"} />
-          </Button>
-        </Tooltip>
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <Tooltip
-          id="instagram-facebook"
-          title="Follow us on facebook"
-          placement={window.innerWidth > 959 ? "top" : "left"}
-          classes={{ tooltip: classes.tooltip }}
-        >
-          <Button
-            color="transparent"
-            href="#"
-            className={classes.navLink}
-          >
-            <i className={classes.socialIcons + " fab fa-facebook"} />
-          </Button>
-        </Tooltip>
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <Tooltip
-          id="instagram-tooltip"
-          title="Follow us on instagram"
-          placement={window.innerWidth > 959 ? "top" : "left"}
-          classes={{ tooltip: classes.tooltip }}
-        >
-          <Button
-            color="transparent"
-            href="#"
-            className={classes.navLink}
-          >
-            <i className={classes.socialIcons + " fab fa-instagram"} />
-          </Button>
-        </Tooltip>
-      </ListItem>
-    </List>
+    <>
+    </>
+    // <List className={classes.list}>
+    //   <ListItem className={classes.listItem}>
+    //     {/*<Tooltip title="Delete">
+    //       <IconButton aria-label="Delete">
+    //         <DeleteIcon />
+    //       </IconButton>
+    //     </Tooltip>*/}
+    //     <Tooltip
+    //       id="instagram-twitter"
+    //       title="Follow us on twitter"
+    //       placement={window.innerWidth > 959 ? "top" : "left"}
+    //       classes={{ tooltip: classes.tooltip }}
+    //     >
+    //       <Button
+    //         href="#"
+    //         color="transparent"
+    //         className={classes.navLink}
+    //       >
+    //         <i className={classes.socialIcons + " fab fa-twitter"} />
+    //       </Button>
+    //     </Tooltip>
+    //   </ListItem>
+    //   <ListItem className={classes.listItem}>
+    //     <Tooltip
+    //       id="instagram-facebook"
+    //       title="Follow us on facebook"
+    //       placement={window.innerWidth > 959 ? "top" : "left"}
+    //       classes={{ tooltip: classes.tooltip }}
+    //     >
+    //       <Button
+    //         color="transparent"
+    //         href="#"
+    //         className={classes.navLink}
+    //       >
+    //         <i className={classes.socialIcons + " fab fa-facebook"} />
+    //       </Button>
+    //     </Tooltip>
+    //   </ListItem>
+    //   <ListItem className={classes.listItem}>
+    //     <Tooltip
+    //       id="instagram-tooltip"
+    //       title="Follow us on instagram"
+    //       placement={window.innerWidth > 959 ? "top" : "left"}
+    //       classes={{ tooltip: classes.tooltip }}
+    //     >
+    //       <Button
+    //         color="transparent"
+    //         href="#"
+    //         className={classes.navLink}
+    //       >
+    //         <i className={classes.socialIcons + " fab fa-instagram"} />
+    //       </Button>
+    //     </Tooltip>
+    //   </ListItem>
+    // </List>
   );
 }
